55c76b72bdb2f6b2bcadc251cb1115989100e768
divinojack <115471906+divinojack@users.noreply.github.com>
2022-12-08 23:42:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
-
+#include "lib.h"
+bool verifica(char a){
+    int x;
+    x=(int)a;
+    if(x>=65 && x<=90 || x>=97 && x<=122) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+char lettera(char a){
+    int x;
+    x=(int)a;
+    int y=0;
+    if(x>=65 && x<=90){
+        y=x+32;
+    }else if(x>=97 && x<=122){
+        y= x-32;
+    }
+    return y;
+}
